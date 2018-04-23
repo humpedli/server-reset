eeebe6f7218e52297efa2a0f8c8e648ea4b96840
Tamas Kinsztler <hump@hump.hu>
2018-04-24 00:29:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const express = require('express');
+cost wpi = require('wiring-pi');
+
+wpi.setup('wpi');
 
 // Constants
 const PORT = 12000;
@@ -10,6 +13,10 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello world\n');
+  wpi.pinMode(4, wpi.OUTPUT);
+  setTimeout(function() {
+    wpi.digitalWrite(4, 1);
+  }, 300);
 });
 
 app.listen(PORT, HOST);
