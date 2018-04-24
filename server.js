4e5d952bf1d1c59724c7a82e65def064d681b141
humpedli <hump@hump.hu>
2018-04-24 15:10:33 +0200

    Changes

--- server.js
+++ server.js
@@ -4,21 +4,31 @@ const express = require('express');
 const gpio = require('rpi-gpio');
 const gpiop = gpio.promise
 
-// Constants
 const PORT = 12000;
 const HOST = '0.0.0.0';
-
-// App
 const app = express();
 
+console.log(`Initializing GPIO 11 port...`)
 gpiop.setup(11, gpio.DIR_HIGH);
 
 app.get('/', (req, res) => {
-  gpio.write(11, false);
-  setTimeout(() => {
-    gpio.write(11, true);
-  }, 300);
-  res.send('Hello world\n');
+	res.sendFile('index.html');
+});
+
+app.get('/reset', (req, res) => {
+	console.log(`Sending signal...`)
+  	gpio.write(11, false);
+
+  	setTimeout(() => {
+    	gpio.write(11, true);
+    	console.log(`Server reset signal sent!`)
+  	}, 300);
+
+  	res.redirect('/success');
+});
+
+app.get('/success', (req, res) => {
+	res.sendFile('success.html');
 });
 
 app.listen(PORT, HOST);
